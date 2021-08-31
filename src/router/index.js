0493937f6d2578a348410c8a4fbbb49acc7e711b
MyintSanWin <jackmee24680@gmail.com>
2021-08-31 18:13:10 +0630

    after some changes

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+import Detail from "../views/Detail.vue"
+import Create from "../views/Create.vue"
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path :'/posts/:id',
+    name:'Detail',
+    component:Detail,
+    props:true
+  },
+  {
+    path:'/create',
+    name:"Create",
+    component:Create
+  }
+  
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="home">
+ <div v-if="error">{{error}}</div>
+  <div v-if="posts.length>0"><PostsList :posts="posts"></PostsList></div>
+  <div v-else>
+    <spinner></spinner>
+  </div>
+  </div>
+  
+
+
+</template>
+
+<script>
+
+import PostsList from '../components/PostsList.vue'
+import Spinner from '../components/Spinner.vue';
+import getPosts from "../composables/getPosts"
+
+// @ is an alias to /src
+
+
+export default {
+  components: {PostsList, Spinner},
+  setup(){
+    //composable function
+    let {posts,error,load}=getPosts();//posts,error,load
+      load();
+    return {posts,error}
+  }
+  
+  
+}
+</script>
+
+<style>
+ .home {
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 10px;
+  }
+  .layout{
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    gap:20px;
+  }
+</style>
+
